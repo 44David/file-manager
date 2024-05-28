613f21be79d760a3a52386dfec5b5990a7117e63
David Soane <david.soane@outlook.com>
2024-05-27 20:27:31 -0600

    added cli packages

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+
+struct Args {
+    #[arg(short, long)]
+    name: String,
+
+    count: u8,
+}
+
 fn main() {
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
 }
